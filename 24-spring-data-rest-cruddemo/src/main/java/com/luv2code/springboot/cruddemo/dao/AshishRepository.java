4701589475b07f03ea52d21ba1a5c73fffea08e4
cs2252 <chandansingh1896@gmail.com>
2020-09-03 14:46:28 +0530

    commmited

---
+++ 24-spring-data-rest-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/AshishRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.cruddemo.entity.Ashish;
+
+public interface AshishRepository extends JpaRepository<Ashish, Integer> {
+
+}
